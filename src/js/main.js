36bf7a155fc48d1c0fddb54b33635bab8deecd34
kowyzu <kowyzu@gmail.com>
2025-03-11 21:15:18 +0100

    feat: navbar styling

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,8 @@
 import "../scss/styles.scss";
 import * as bootstrap from "bootstrap";
+
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fas);
+dom.watch(); // Aplikuje ikony na HTML automaticky
