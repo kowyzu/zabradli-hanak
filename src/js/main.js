9d3b6be2bc3959673a5ec126d0a86173d6262fed
kowyzu <kowyzu@gmail.com>
2025-03-13 23:23:19 +0100

    feat: js of carousel initializing

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,28 @@ import { fas } from "@fortawesome/free-solid-svg-icons";
 
 library.add(fas);
 dom.watch(); // Aplikuje ikony na HTML automaticky
+
+//
+// Introduction Carousel
+// ---------------------
+console.log("TODO - umoznit posouvani pomoci bttns a pak pokracovat v cycle");
+
+const carouselIntroduction = new bootstrap.Carousel("#carouselIntroduction", {
+  interval: 3500,
+});
+
+window.addEventListener("load", (event) => {
+  carouselIntroduction.cycle();
+});
+
+// const carouselBtns = document.querySelectorAll(".carousel-btn");
+
+// carouselBtns.forEach((carouselBtn) => {
+//   carouselBtn.addEventListener("click", () => {
+//     carouselIntroduction.next();
+//   });
+// });
+
+// introText.addEventListener("click", () => {
+//   carouselIntroduction.next();
+// });
