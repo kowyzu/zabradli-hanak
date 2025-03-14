f5ff632eb7c4eb6d3d1c2e4fa5c701cc0a57aba4
kowyzu <kowyzu@gmail.com>
2025-03-14 20:38:09 +0100

    feat: complete carousel js usage in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -10,24 +10,20 @@ dom.watch(); // Aplikuje ikony na HTML automaticky
 //
 // Introduction Carousel
 // ---------------------
-console.log("TODO - umoznit posouvani pomoci bttns a pak pokracovat v cycle");
-
 const carouselIntroduction = new bootstrap.Carousel("#carouselIntroduction", {
-  interval: 3500,
+  interval: 4000,
+  pause: false,
 });
 
 window.addEventListener("load", (event) => {
   carouselIntroduction.cycle();
 });
 
-// const carouselBtns = document.querySelectorAll(".carousel-btn");
+const carouselBtns = document.querySelectorAll(".carousel-btn");
 
-// carouselBtns.forEach((carouselBtn) => {
-//   carouselBtn.addEventListener("click", () => {
-//     carouselIntroduction.next();
-//   });
-// });
-
-// introText.addEventListener("click", () => {
-//   carouselIntroduction.next();
-// });
+carouselBtns.forEach((carouselBtn) => {
+  carouselBtn.addEventListener("click", () => {
+    let targetIndex = carouselBtn.getAttribute("data-bs-slide-to");
+    carouselIntroduction.to(targetIndex);
+  });
+});
