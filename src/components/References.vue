2946caab03b7fa3ca4f2b636637645c042efa22d
kowyzu <kowyzu@gmail.com>
2025-04-05 20:50:30 +0200

    feat: basic References layout

--- src/components/References.vue
+++ src/components/References.vue
@@ -1,23 +1,32 @@
 <template>
-  <section id="references" class="p-5 m-5">
+  <section id="references" class="p-md-5 p-2 text-center">
+    <h1 class="p-4">Vybrané reference</h1>
     <div class="container text-center">
-      <div class="row">
-        <div class="col">
-          <img src="../imgs/galery-imgs/small/104.jpg" alt="">
-        </div>
-        <div class="col">
-          <div class="container text-start">
-            <div class="row">
-              <h1>1. reference</h1>
-              <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque facere cum officiis
-                harum sed vel
-                in sapiente aut numquam cupiditate voluptatibus ea, tenetur blanditiis similique. Odit voluptas nihil
-                soluta non?</p>
+      <div class="row row-cols-1">
+        <div class="col-md-3 col-4">
+          <div class="reference-preview-img">
+            <div class="row reference-preview-img-first">
+              <img class="p-3" src="../imgs/galery-imgs/big/125.jpg" alt="galerry img of rail">
             </div>
-            <div id="galerry-preview" class="row">
-              <div v-for="(img) in testGalleryJson" :key="img.id" class="col">
-                <img :src="'../imgs/galery-imgs/big/' + img.id + '.jpg'" class="img-fluid" alt="galerry img of rail">
-              </div>
+          </div>
+          <div class="reference-preview-img-second">
+            <div class="row reference-preview-img">
+              <img class="p-3" src="../imgs/galery-imgs/big/134.jpg" alt="galerry img of rail">
+            </div>
+          </div>
+        </div>
+        <div class="col-md-9 col-8 reference-detail px-5 pt-2 pb-5">
+          <div class="row text-start">
+            <h2 class="pt-3">1. reference</h2>
+            <hr class="references-underline">
+            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque facere cum officiis
+              harum sed vel
+              in sapiente aut numquam cupiditate voluptatibus ea, tenetur blanditiis similique. Odit voluptas nihil
+              soluta non?</p>
+          </div>
+          <div id="galerry-preview" class="row row-cols-md-4 row-cols-1 gy-3 gx-3">
+            <div v-for="(img) in testGalleryJson" :key="img.id" class="col">
+              <img :src="'../imgs/galery-imgs/big/' + img.id + '.jpg'" class="img-fluid" alt="galerry img of rail">
             </div>
           </div>
         </div>
@@ -29,7 +38,7 @@
 <script>
 import { useTemplateRef } from 'vue';
 import test_gallery_JSON from '../objects/test-reference-gallery.json'
-console.log(test_gallery_JSON);
+
 
 export default {
   data() {
--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,7 @@ import { createApp } from "vue";
 import App from "../App.vue";
 
 library.add(fas);
-dom.watch(); // Aplikuje ikony na HTML automaticky
+dom.watch(); // Applies icons to HTML automatically.
 
 createApp(App).mount("#app");
 //
@@ -35,76 +35,3 @@ carouselBtns.forEach((carouselBtn) => {
     carouselIntroduction.to(targetIndex);
   });
 });
-
-//
-// On scroll animation
-// -------------------
-document.addEventListener("DOMContentLoaded", () => {
-  const waypoint = document.querySelector(".waypoint");
-  const processStepsFirstRow = document.querySelectorAll(
-    ".process-step-first-row"
-  );
-  const processStepsSecondRow = document.querySelectorAll(
-    ".process-step-second-row"
-  );
-
-  processStepsFirstRow.forEach((processStepFirst, index) => {
-    setTimeout(() => {
-      processStepFirst.classList.add("show");
-    }, index * 500); // postupné zobrazování
-  });
-
-  const observer = new IntersectionObserver(
-    (entries) => {
-      if (entries[0].isIntersecting) {
-        processStepsSecondRow.forEach((processStepSecond, index) => {
-          setTimeout(() => {
-            processStepSecond.classList.add("show");
-          }, index * 500); // postupné zobrazování
-        });
-        observer.disconnect(); // zajistí, že se animace spustí jen jednou
-      }
-    },
-    { threshold: 1.0 }
-  );
-
-  observer.observe(waypoint);
-});
-
-//
-// Gallery show more
-// -----------------
-
-// const gallerySection = document.getElementById("gallery");
-// const showBtn = document.querySelector(".btn-show-more");
-// const hideBtn = document.querySelector(".btn-show-less");
-// const imgsToShow = document.querySelectorAll(".more-img");
-// const showMoreImg = document
-//   .querySelector(".show-more-container")
-//   .querySelector("img");
-
-// showBtn.addEventListener("click", () => {
-//   imgsToShow.forEach((imgToShow) => {
-//     imgToShow.classList.remove("hidden");
-//   });
-
-//   showBtn.classList.add("hidden");
-//   hideBtn.classList.remove("hidden");
-//   showMoreImg.classList.remove("show-more-img");
-// });
-
-// hideBtn.addEventListener("click", () => {
-//   imgsToShow.forEach((imgToShow) => {
-//     imgToShow.classList.add("hidden");
-//   });
-
-//   showBtn.classList.remove("hidden");
-//   hideBtn.classList.add("hidden");
-//   showMoreImg.classList.add("show-more-img");
-
-//   gallerySection.scrollIntoView({
-//     behavior: "instant",
-//     block: "start",
-//     inline: "nearest",
-//   });
-// });
