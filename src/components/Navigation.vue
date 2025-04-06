c12b044dddd107c7cdd9a57fab73782ea748f076
kowyzu <kowyzu@gmail.com>
2025-04-06 22:05:35 +0200

    feat: complete navbar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -6,18 +6,16 @@ import logo from '../imgs/logo_zabradli_hanak.png'
   <header class="sticky-top">
     <nav class="navbar navbar-expand-lg shadow-sm"> <!--bg-body-tertiary-->
       <div class="container-fluid">
-        <div class="container">
-          <a class="navbar-brand" href="#">
-            <img class="img-logo" :src="logo" alt="Logo Zábradlí Hanák">
-          </a>
-        </div>
+        <a class="navbar-brand" href="#">
+          <img class="img-logo" :src="logo" alt="Logo Zábradlí Hanák">
+        </a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse flex-grow-0" id="navbarSupportedContent">
           <ul class="navbar-nav">
             <li class="nav-item">
               <a class="nav-link" aria-current="page" href="#">Úvod</a> <!--mozno pouzit class "active"-->
@@ -37,7 +35,7 @@ import logo from '../imgs/logo_zabradli_hanak.png'
           </ul>
         </div>
 
-        <div class="container-fluid navbar-contact-details">
+        <div class="navbar-contact-details d-lg-block d-none">
           <ul class="navbar-nav">
             <li>Marek Hanák</li>
             <li><i class="fa-solid fa-phone icon-contact-details fa-fw" style="color: #a40c24;"></i> 606 146 985</li>
