a7dcc8057b57107d8c4fddfe7684c426b059fbd9
kowyzu <kowyzu@gmail.com>
2025-04-23 23:30:28 +0200

    feat: start setup mail sending when contact form is submitted

--- backend/index.php
+++ backend/index.php
@@ -1,2 +1,15 @@
+<?php 
+  $to = 'zuza250@seznam.cz';
+  $subject = 'test zabradli';
+  $message = 'hello test';
+  $header = 'From: webmaster@yourdomain.cz';
+  $success = mail($to, $subject, $message, $header);
 
- <?php echo $_POST["name"]; ?><br>
+  var_dump($success);
+
+  if ($success === true) {
+    echo 'E‑mail byl odeslán!';  // Email was sent
+  } else {
+    echo 'Odeslání e‑mailu se nezdařilo.';  // Failed to send
+  }
+?>
