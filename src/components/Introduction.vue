7dc2a3281ae569ed561565437d8308b400f05618
kowyzu <kowyzu@gmail.com>
2025-06-02 22:56:58 +0200

    feat: seo meta tags and refactor

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -10,7 +10,6 @@ import thirdImg from '../imgs/introduction-imgs/third.webp'
       <div class="row justify-content-md-left align-items-xl-center">
         <div class="col-lg-7 col-xxl-6 introduction-carousel p-0">
           <div id="carouselIntroduction" class="carousel slide shape-mask carousel-fade">
-            <!--data-bs-ride="carousel" -->
             <div class="carousel-inner">
               <div class="carousel-indicators justify-content-start ms-3 mb-3">
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
@@ -35,11 +34,9 @@ import thirdImg from '../imgs/introduction-imgs/third.webp'
 
         <div
           class="col-lg-5 offset-xxl-1 col-xxl-4 introduction-text p-3 py-5 p-md-5 p-lg-4 p-xl-5 p-xxl-0 text-center text-sm-start">
-          <!-- text to update? -->
           <h1 class="mb-xxl-3 mb-xl-2">
             Zakázková výroba schodišť a zábradlí
           </h1>
-          <!-- text to update -->
           <p class="mb-xl-3">
             Specializujeme se na <strong>nerez, hliník a sklo</strong>. Precizní řemeslné zpracování a individuální
             přístup jsou pro nás
